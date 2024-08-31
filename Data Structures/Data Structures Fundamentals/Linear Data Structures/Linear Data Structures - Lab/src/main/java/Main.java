fd335cc733dfc6b75b8cc57170bf2e7723d64766
Antoan Metodiev <Boksnem@abv.bg>
2024-08-31 23:58:25 +0300

    add ArrayList implementation

---
+++ Data Structures/Data Structures Fundamentals/Linear Data Structures/Linear Data Structures - Lab/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import implementations.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("1");
+        names.add("2");
+        names.add("3");
+        names.add("4");
+
+
+        System.out.println(names.indexOf("4"));
+
+
+    }
+}
