64c056085192aed92763fa0b30b9b766d5b64abf
Antoan Metodiev <Boksnem@abv.bg>
2025-01-15 22:30:23 +0200

    implement MaxHeap binary tree

--- Data Structures/Data Structures Fundamentals/Heaps, BST/Heaps, BST - Lab/src/main/java/implementations/MaxHeap.java
+++ Data Structures/Data Structures Fundamentals/Heaps, BST/Heaps, BST - Lab/src/main/java/implementations/MaxHeap.java
@@ -3,23 +3,43 @@ package implementations;
 import interfaces.Heap;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
+    private List<E> elements;
+
+    public MaxHeap() {
+        this.elements = new ArrayList<>();
+    }
 
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
+    }
 
+    private void heapifyUp(int index) {
+        int parentIndex = (index - 1) / 2;
+        while (index > 0 && parentIsLess(index, parentIndex)) {
+            Collections.swap(this.elements, index, parentIndex);
+            index = parentIndex;
+        }
+    }
+
+    private boolean parentIsLess(int lastIndex, int parentIndex) {
+        return this.elements.get(lastIndex).compareTo(this.elements.get(parentIndex)) > 0;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (this.elements.isEmpty()) throw new IllegalArgumentException();
+        return this.elements.get(0);
     }
 }
